cfeecab149599be132cd6517207cb54147980cd7
Clara Ballester maymo <clballes@cbr2s6.42barcelona.com>
2023-03-20 18:02:40 +0100

    translate

--- src/kw_dict.py
+++ src/kw_dict.py
@@ -1,7 +1,9 @@
 import json
+from googletrans import Translator, constants
+from pprint import pprint
 
 # GUARDAMOS EN LA VARIABLE ARTICLE_KW EL DICCIONARIO CON EL ID Y SUS KEYWORDS
-with open("articles.json") as dict_article:
+with open("../json/articles.json") as dict_article:
     articles = json.load(dict_article)
     article_kw = {}
     for i in articles:
@@ -10,11 +12,34 @@ with open("articles.json") as dict_article:
 dict_article.close()
 
 # GUARDAMOS EN LA VARIABLE VIDEOS_KW EL DICCIONARIO CON EL ID Y SUS KEYWORDS
-with open("videos.json") as dict_videos:
-    print("hola")
+with open("../json/videos.json") as dict_videos:
     videos = json.load(dict_videos)
     videos_kw = {}
     for i in videos:
         videos_kw[i] = videos[i]["keywords"]
-    print(videos_kw)
+    # print(videos_kw)
 dict_article.close()
+
+# init the Google API translator
+translator = Translator()
+# translate a spanish text to english text (by default)
+for article in articles:
+    # print(article)
+    articles_list =[]
+    for value in articles[article]['keywords']:
+        translation = translator.translate(value)
+        text_eng = (translation.text)
+        articles_list.append(text_eng)
+    articles[article]['keywords'] = articles_list
+    # print(articles.list)
+
+
+for video in videos:
+    print(video)
+    video_list=[]
+    for value in videos[video]['keywords']:
+        translation = translator.translate(value)
+        text_eng = (translation.text)
+        video_list.append(text_eng)
+    videos[video]['keywords'] = video_list
+    # print(video_list)
