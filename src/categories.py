66210b7a1b981432a2f7c4ac40c3384802008f56
Juan Queiroz Perez <jqueiroz@cbr3s5.42barcelona.com>
2023-03-20 17:59:55 +0100

    fix: set de categorias de los dos json

--- src/categories.py
+++ src/categories.py
@@ -14,4 +14,16 @@ with open('../json/articles.json') as f:
                 categories.add(category) 
     f.close()
 
+with open('../json/videos.json') as f:
+
+    articles = json.load(f)
+
+    for i in articles:
+        categoryList = articles[i]["categoriaIAB"]
+        for j in range(len(categoryList)):
+            category = str(categoryList[j]["class"])
+            if category not in categories:
+                categories.add(category) 
+    f.close()
+
 print(categories)
