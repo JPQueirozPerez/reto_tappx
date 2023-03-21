16e1968ec1aedf8e384ddbd09949ba482df22c91
Gerard Gispert Carreras <ggispert@cbr10s2.42barcelona.com>
2023-03-21 16:08:47 +0100

    updated to a better version

--- src/word2vec.py
+++ src/word2vec.py
@@ -2,24 +2,22 @@ from gensim.models import KeyedVectors
 from sklearn.metrics.pairwise import cosine_similarity
 
 #Returns a dictionary with every keyword punctuated with every category via word2vec
-def punctuate_artcls(dict_artcl, categories):
+def punctuate_srcs(dict_srcs, categories):
 	word_vectors = KeyedVectors.load_word2vec_format('/Users/ggispert/Desktop/keyed/GoogleNews-vectors-negative300.bin.gz', binary=True)
-	for artcl in dict_artcl:
+	for src in dict_srcs:
 		dict_aux = {}
 		for cat in categories:
 				dict_aux[cat] = float(0)
-		for kw in dict[artcl]:
+		for kw in dict[src]:
 			v_kw = 0
 			for kw_pt in kw.split():
-				if len(kw_pt > 2)
-					v_kw += word_vectors[kw_pt]
+				v_kw += word_vectors[kw_pt]
 			for cat in categories:
 				v_cat = 0
 				for cat_pt in cat.split():
-					if len(cat_pt) > 2:
-						v_cat += word_vectors[cat_pt]
+					v_cat += word_vectors[cat_pt]
 				dict_aux[cat] += float(cosine_similarity([v_kw], [v_cat]))
 		for cat in categories:
-			dict_aux[cat] /= len(dict_artcl[artcl])
-		dict_artcl[artcl] = dict_aux
-	return dict_artcl
+			dict_aux[cat] /= len(dict_src[src])
+		dict_src[src] = dict_aux
+	return dict_srcs
