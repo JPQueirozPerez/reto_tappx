c4773b97cd442d1640fab07ec75a46b1e5a63b8f
Juan Queiroz Perez <jqueiroz@cbr11s3.42barcelona.com>
2023-03-22 16:03:59 +0100

    feat: crear json

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from kwArtcl import *
 from kwVideos import *
 import categories
+import json
 
 categories = categories.list_categories()
 print(categories)
@@ -8,5 +9,18 @@ print(categories)
 articles = kwArt()
 print(articles)
 
-videos = kwVideos()
-print(videos)
+#videos = kwVideos()
+#print(videos)
+
+
+def writeToJSONFile(path, fileName, data):
+    filePathNameWExt = './' + path + '/' + fileName + '.json'
+    with open(filePathNameWExt, 'w') as fp:
+        json.dump(data, fp)
+
+
+# Example
+#data = categories
+#data['key'] = 'value'
+
+#writeToJSONFile('./','file-name2',data)
